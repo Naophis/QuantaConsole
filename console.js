5c962b2fa4fac4b23f38dd7adfb1a10961c74ecd
starknighthood <nao1288stusj@gmail.com>
2017-10-19 01:30:26 +0900

    接続を切れるように修正

--- console.js
+++ console.js
@@ -9,38 +9,54 @@ var msg = "";
 io.set('heartbeat timeout', 5000);
 io.set('heartbeat interval', 5000);
 
+let firstConnect = true;
 io.sockets.on('connection', function (socket) {
 	globalScoket = socket;
-	let interval;
-	// sp = new serialPort.SerialPort("COM7", {
-	// 	baudrate: 230400,
-	// 	parser: serialPort.parsers.readline("\n")
-	// });
-	// socket.on('disconnect', function () {
-	// 	sp.close(function () {
-	// 		log("closed");
-	// 	});
-	// 	clearInterval(interval);
-	// });
+	try {
+		if (firstConnect) {
+			sp = new serialPort.SerialPort("COM3", {
+				baudrate: 230400,
+				parser: serialPort.parsers.readline("\n")
+			});
+			firstConnect = false;
+		}
+	} catch (e) {
+		console.log(e)
+	}
+	socket.on('disconnect', function () {
+		sp.close(function () {
+			log("closed");
+			firstConnect = true;
+		});
+	});
+	socket.on('mydisconnect', function () {
+		sp.close(function () {
+			log("closed");
+			firstConnect = true;
+		});
+	});
+	socket.on('myconnect', function () {
+		if (firstConnect) {
+			sp = new serialPort.SerialPort("COM3", {
+				baudrate: 230400,
+				parser: serialPort.parsers.readline("\n")
+			});
+			firstConnect = false;
+		}
+	});
 	// sp.on('data', function (data) {
 	// 	if (globalScoket) {
 	// 		globalScoket.emit('message', {
 	// 			message: data
-	// 		}, function (res) { });
+	// 		}, function (res) {});
 	// 	}
 	// 	console.log(data);
 	// });
-	// interval = setInterval(function () {
-	// 	sp.write('hello', function () {
-	// 		console.log("send");
-	// 	});
-	// }, 1000);
 	socket.on("update", function (res) {
 		if (res) {
 			readFile("output.json", function (json) {
 				var savedDataList = JSON.parse(json).data;
 				var head = true;
-
 				for (var i = 0; i < savedDataList.length; i++) {
 					if (savedDataList[i].id === res.id) {
 						savedDataList[i] = res;
@@ -49,6 +65,9 @@ io.sockets.on('connection', function (socket) {
 						}
 						head = false;
 						writeFile("output.json", JSON.stringify(saveData, null, '\t'));
+						sp.write(`{${res.id}:${res.value}}`, function () {
+							console.log("send");
+						});
 					}
 				}
 				if (head) {
@@ -66,15 +85,7 @@ io.sockets.on('connection', function (socket) {
 			readFile("output.json", function (data) {
 				socket.emit('list', JSON.parse(data), function (res) {});
 			});
-		} else {
-
 		}
-		// console.log(data.target);
-		// let key = data.target ? data.target : "10000";
-		// let val = data.value; // data.value;
-		// sp.write(`{${key}:${val}}`, function () {
-		// 	console.log("send");
-		// });
 	});
 });
 //ファイル読み込み関数
--- js/index.js
+++ js/index.js
@@ -10,17 +10,56 @@ app.controller('HelloWorldController', ['$scope', function ($scope) {
         let target = $.isNumeric(id) ? id : null;
         socket.emit("load", id);
     }
+    $scope.connect = function (id) {
+        let target = $.isNumeric(id) ? id : null;
+        socket.emit("load", id);
+    }
+    $scope.disconnect = function (id) {
+        let target = $.isNumeric(id) ? id : null;
+        socket.emit("disconnect", id);
+    }
+
+    $scope.connect = function () {
+        socket.emit("myconnect");
+    }
+
+    $scope.disconnect = function () {
+        socket.emit("mydisconnect");
+    }
     $scope.update = function (id) {
-        console.log(id);
         var map = $scope.paramList.filter(function (param) {
             return param.id === id;
         });
-
-        socket.emit("update", {
-            id: map[0].id,
-            name: map[0].name,
-            value: map[0].value,
-        });
+        if ($.isNumeric(map[0].value)) {
+            socket.emit("update", {
+                id: map[0].id,
+                name: map[0].name,
+                value: map[0].value,
+            });
+            $.toast({
+                text: "send",
+                showHideTransition: 'slide', // It can be plain, fade or slide
+                bgColor: 'blue', // Background color for toast
+                textColor: '#eee', // text color
+                allowToastClose: false, // Show the close button or not
+                hideAfter: 1000, // `false` to make it sticky or time in miliseconds to hide after
+                stack: 5, // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                textAlign: 'center', // Alignment of text i.e. left, right, center
+                position: 'top-center' // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+            });
+        } else {
+            $.toast({
+                text: "invalid",
+                showHideTransition: 'slide', // It can be plain, fade or slide
+                bgColor: 'red', // Background color for toast
+                textColor: '#eee', // text color
+                allowToastClose: false, // Show the close button or not
+                hideAfter: 1000, // `false` to make it sticky or time in miliseconds to hide after
+                stack: 5, // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                textAlign: 'center', // Alignment of text i.e. left, right, center
+                position: 'top-center' // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+            });
+        }
 
     }
     socket.on("list", function (res) {
